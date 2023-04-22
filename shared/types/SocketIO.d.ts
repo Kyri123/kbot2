957531192335863ae110bfabc419f429a6b66f36
Oliver <olli143@live.de>
2023-04-22 19:20:27 +0200

    init template

--- shared/types/SocketIO.d.ts
+++ shared/types/SocketIO.d.ts
@@ -1,12 +1,7 @@
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
-import {
-	IMO_Blueprint,
-	IMO_BlueprintPack
-}                           from "./MongoDB";
 
 interface IEmitEvents extends DefaultEventsMap {
-	BlueprintUpdated : ( Blueprint : IMO_Blueprint ) => void;
-	BlueprintPackUpdated : ( Blueprint : IMO_BlueprintPack ) => void;
+	example : () => void;
 }
 
 type IListenEvents = IEmitEvents;
